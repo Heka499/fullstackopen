a2d197810ec924fed8a53046108e2da4c64ccb42
Heikki Jarvinen <heikki.jarvinen1@edu.turkuamk.fi>
2024-05-20 13:40:58 +0300

    chore: Add React and Vite configuration files and initial code

---
+++ osa1/part1/src/App.jsx
@@ -0,0 +1,29 @@
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old.</p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by 
+      <a href="https://github.com/Heka499">Heka499</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const friends = [ 'Leevi', 'Venla']
+
+  return (
+    <div>
+      <p>{friends}</p>
+    </div>
+  )
+}
+
+export default App
