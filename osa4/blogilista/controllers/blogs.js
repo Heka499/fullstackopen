f0143aa8de64b463f78d40cbfbdb2c997ad898f9
Heikki Järvinen <heikki.jarvinen1@edu.turkuamk.fi>
2024-06-06 15:16:44 +0300

    4.14 done

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -33,5 +33,23 @@ blogRouter.delete('/:id', async (request, response) => {
     response.status(204).end()
 })
 
+blogRouter.put('/:id', async (request, response) => {
+    const body = request.body
+
+    if (!body) {
+        return response.status(400).json({ error: 'body missing' })
+    }
+
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+    }
+
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    response.json(updatedBlog)
+})
+
   
 module.exports = blogRouter
