60ebe0f774aa89cbc6b7b9bff0b2ae62d3e7e606
Heikki Järvinen <heikki.jarvinen1@edu.turkuamk.fi>
2024-06-06 15:10:56 +0300

    4.13 done

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -85,6 +85,22 @@ test('blog without title or url is not added', async () => {
     assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
 
+test('a blog can be deleted', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+
+    const titles = blogsAtEnd.map(blog => blog.title)
+
+    assert(!titles.includes(blogToDelete.title))
+})
 
 after(async () => {
     await mongoose.connection.close()
