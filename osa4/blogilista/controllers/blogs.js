4c8773c2f0c9d0debe7e19489d7c7519d16f42ef
Heikki Jarvinen <heikki.jarvinen1@edu.turkuamk.fi>
2024-06-07 18:25:06 +0300

    4.17 done

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,13 +1,18 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+      .find({}).populate('user', { username: 1, name: 1 })
+      
     response.json(blogs)
 })
   
 blogRouter.post('/', async (request, response) => {
   const body = request.body
+
+  const user = await User.findById(body.userId)
 
   if (!body.title || !body.url) {
     return response.status(400).json({ error: 'title or url missing' })
@@ -22,9 +27,13 @@ blogRouter.post('/', async (request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes,
+    user: user._id
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
--- osa4/blogilista/models/blog.js
+++ osa4/blogilista/models/blog.js
@@ -2,12 +2,13 @@ const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
     title: String,
-    author: {
+    author: String,
+    url: String,
+    likes: Number,
+    user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
-    },
-    url: String,
-    likes: Number
+    }
 })
 
 blogSchema.set('toJSON', {
