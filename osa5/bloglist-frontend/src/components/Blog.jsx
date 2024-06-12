1ae3abbabd615f755b0bd9215205ba2c5e9d441e
Heikki Järvinen <heikki.jarvinen1@edu.turkuamk.fi>
2024-06-12 16:55:34 +0300

    5.7 done

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  if (!visible) {
+    return (
+      <div style={blogStyle}>
+        <p>{blog.title} by {blog.author} <button onClick={toggleVisibility}>view</button></p>
+      </div>  
+    )
+  } else {
+    return (
+      <div style={blogStyle}>
+        <p>Title: {blog.title} <button onClick={toggleVisibility}>hide</button> </p>
+        <p>Author: {blog.author}</p>
+        <p>Link: {blog.url}</p>
+        <p>Likes: {blog.likes} <button onClick={console.log("click")}>Like</button> </p>
+      </div>  
+    )
+  }
+}
 
 export default Blog
