00920de231923f75021f86d2f463324066746993
Heikki Järvinen <heikki.jarvinen1@edu.turkuamk.fi>
2024-06-13 14:06:43 +0300

    5.8 done

--- osa5/bloglist-frontend/src/services/blogs.js
+++ osa5/bloglist-frontend/src/services/blogs.js
@@ -21,8 +21,14 @@ const create = async newObject => {
   return response.data
 }
 
+const update = (id, newObject) => {
+  const request = axios.put(`${ baseUrl }/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
 export default {
   getAll,
   setToken,
-  create
+  create,
+  update
 }
