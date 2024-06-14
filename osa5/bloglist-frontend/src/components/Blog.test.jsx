09b7fcc1f38bf4c82927152e3a737eb8c136b756
Heikki Jarvinen <heikki.jarvinen1@edu.turkuamk.fi>
2024-06-14 19:08:14 +0300

    5.15 done

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -65,3 +65,37 @@ test('clicking the button shows more info', async () => {
     expect(addedBy).toBeDefined()
 
 })
+
+test('clicking the like button twice calls the event handler twice', async () => {
+    const blog = {
+        title: 'Component testing is done with react-testing-library',
+        author: 'Test Author',
+        url: 'https://www.example.com',
+        likes: 5,
+        user: {
+            name: 'Test User',
+            username: 'testuser'
+        }
+    }
+
+    const testUser = {
+        name: 'Test User',
+        username: 'testuser'
+    }
+
+    const mockHandler = vi.fn()
+
+    render(
+        <Blog blog={blog} user={testUser} like={mockHandler} />
+    )
+
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+
+    const likeButton = screen.getByText('Like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
+})
