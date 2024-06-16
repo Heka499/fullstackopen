3df3a731bb719c11aa52f2269b0b7c18537839be
Heikki Jarvinen <heikki.jarvinen1@edu.turkuamk.fi>
2024-06-16 20:28:16 +0300

    5.18 done

--- osa5/bloglist-frontend_tests/tests/blog_app.spec.js
+++ osa5/bloglist-frontend_tests/tests/blog_app.spec.js
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const exp = require('constants')
 
 describe('Blog app', () => {
     beforeEach(async ({ page, request }) => {
@@ -18,5 +19,23 @@ describe('Blog app', () => {
     await page.waitForSelector('form')
     await expect(page.locator('form')).toBeVisible()
     await expect(page.getByText('log in to application')).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByTestId('username').fill('testuser')
+      await page.getByTestId('password').fill('testpassword')
+      await page.click('button')
+      
+      await expect(page.getByText('Test User logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByTestId('username').fill('testuser')
+      await page.getByTestId('password').fill('wrongpassword')
+      await page.click('button')
+
+      await expect(page.getByText('Test User logged in')).not.toBeVisible()
+    })  
   })
 })
