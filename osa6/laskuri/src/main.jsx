cd3c06be866be9069857db1eb8c010b67a2baa84
Heikki Järvinen <heikki.jarvinen1@edu.turkuamk.fi>
2024-06-18 15:56:12 +0300

    chore: Add Babel and ESLint configuration for React and Jest

--- osa6/laskuri/src/main.jsx
+++ osa6/laskuri/src/main.jsx
@@ -1,38 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import noteReducer from './reducers/noteReducer'
 
 import { createStore } from 'redux'
 
-const counterReducer = (state, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    case 'ZERO':
-      return 0
-    default:
-    return state
-  }
-}
+const store = createStore(noteReducer)
 
-const store = createStore(counterReducer)
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'the app state is in redux store',
+    important: false,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'state changes are made with actions',
+    important: true,
+    id: 2
+  }
+})
 
 function App() {
   return (
     <div>
-      <div>
-        {store.getState()}
-      </div>
-      <button onClick={e => store.dispatch({ type: 'INCREMENT' })}>
-        plus
-      </button>
-      <button onClick={e => store.dispatch({ type: 'DECREMENT' })}>
-        minus
-      </button>
-      <button onClick={e => store.dispatch({ type: 'ZERO' })}>
-        zero
-      </button>
+      <ul>
+        {store.getState().map(note =>
+          <li key={note.id}>
+            {note.content}
+            <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        )}
+      </ul>
     </div>
   )
 }
