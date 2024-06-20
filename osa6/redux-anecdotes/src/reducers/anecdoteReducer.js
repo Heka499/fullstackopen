b4ec160a46a01032fa93d61eb50bc33bfe6d8e2d
Heikki Järvinen <heikki.jarvinen1@edu.turkuamk.fi>
2024-06-20 13:40:02 +0300

    6.15 done

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -37,12 +37,7 @@ const anecdoteSlice = createSlice({
       )
     },
     createAnecdote(state, action) {
-      const content = action.payload
-      state.push({
-        content,
-        id: getId(),
-        votes: 0
-      })
+      state.push(action.payload)
     },
     appendAnecdote(state, action) {
       state.push(action.payload)
