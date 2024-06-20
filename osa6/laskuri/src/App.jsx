555d632a14d08251b19eb9be09d740dae65f92dd
Heikki Järvinen <heikki.jarvinen1@edu.turkuamk.fi>
2024-06-20 13:56:19 +0300

    chore: Update npm dependencies for Redux and Redux Toolkit

--- osa6/laskuri/src/App.jsx
+++ osa6/laskuri/src/App.jsx
@@ -3,13 +3,13 @@ import NewNote from "./components/NewNote"
 import Notes from "./components/Note"
 import VisibilityFilter from "./components/VisibilityFilter"
 import noteService from "./services/notes"
-import { setNotes } from "./reducers/noteReducer"
+import { initializeNotes } from "./reducers/noteReducer"
 import { useDispatch } from "react-redux"
   
 const App = () => {
     const dispatch = useDispatch()
     useEffect(() => {
-        noteService.getAll().then(notes => dispatch(setNotes(notes)))
+        dispatch(initializeNotes())
     }, [])
 
     return (
