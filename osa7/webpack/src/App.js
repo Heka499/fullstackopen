1018669e7e07d442c9b6a80f388de8421978aa5b
Heikki Jarvinen <heikki.jarvinen1@edu.turkuamk.fi>
2024-06-27 16:47:16 +0300

    feat: Add CSS styling to the React app

--- osa7/webpack/src/App.js
+++ osa7/webpack/src/App.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import './index.css'
+
+const useNotes = (url) => {
+    const [notes, setNotes] = useState([])
+    useEffect(() => {
+        axios.get(url).then(response => {
+            setNotes(response.data)
+        })
+    }, [url])
+    return notes
+}
 
 const App = () => {
+    const [counter, setCounter] = useState(0)
+    const [values, setValues] = useState([])
+    const notes = useNotes(BACKEND_URL)
+
+    const handleClick = () => {
+        setCounter(counter + 1)
+        setValues(values.concat(counter))
+    }
+
     return (
-        <div>
-            <h1>Webpack</h1>
+        <div className='container'>
+            hello webpack {counter} clicks
+            <button onClick={handleClick}>
+                press
+            </button>
+            <div>
+                {notes.length} notes on server {BACKEND_URL}
+            </div>
         </div>
     )
 }
