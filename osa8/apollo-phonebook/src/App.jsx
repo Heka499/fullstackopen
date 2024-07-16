486a25418e94c2f1273cb9dcf5c85353b7764ee5
Heikki Järvinen <heikki.jarvinen1@edu.turkuamk.fi>
2024-07-16 15:02:38 +0300

    chore: Handle and display error messages in PersonForm component

--- osa8/apollo-phonebook/src/App.jsx
+++ osa8/apollo-phonebook/src/App.jsx
@@ -2,8 +2,11 @@ import { gql, useQuery } from "@apollo/client";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import { ALL_PERSONS } from "./queries";
+import { useState } from "react";
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null);
+
   const result = useQuery(ALL_PERSONS, {
     pollInterval: 2000,
   });
@@ -12,12 +15,28 @@ const App = () => {
     return <div>loading...</div>;
   }
 
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 10000);
+  };
+
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
-      <PersonForm />
+      <PersonForm setError={notify} />
     </div>
   );
+};
+
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null;
+  }
+
+  return <div style={{ color: "red" }}>{errorMessage}</div>;
 };
 
 export default App;
--- osa8/apollo-phonebook/src/components/PersonForm.jsx
+++ osa8/apollo-phonebook/src/components/PersonForm.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { gql, useMutation } from "@apollo/client";
 import { CREATE_PERSON, ALL_PERSONS } from "../queries";
 
-const PersonForm = () => {
+const PersonForm = ({ setError }) => {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [street, setStreet] = useState("");
@@ -10,6 +10,12 @@ const PersonForm = () => {
 
   const [createPerson] = useMutation(CREATE_PERSON, {
     refetchQueries: [{ query: ALL_PERSONS }],
+    onError: (error) => {
+      const messages = error.graphQLErrors
+        .map((error) => error.message)
+        .join("\n");
+      setError(messages);
+    },
   });
 
   const submit = async (event) => {
