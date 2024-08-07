7921a99e93bde63fc3d6d0c5e3b5570ee451cc29
Heikki Järvinen <heikki.jarvinen1@edu.turkuamk.fi>
2024-08-07 14:59:50 +0300

    9.10 done

--- osa9/patientor-backend/src/index.ts
+++ osa9/patientor-backend/src/index.ts
@@ -1,13 +1,18 @@
 import express from "express";
+import diagnosesRouter from "./routes/diagnoses";
 const app = express();
+const cors = require("cors");
 app.use(express.json());
+app.use(cors());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get("/ping", (_req, res) => {
+app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
   res.send("pong");
 });
+
+app.use("/api/diagnoses", diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
