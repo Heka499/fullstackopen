1358cc3ae5c6c4e58150f207106d5df18167e8bd
Heikki Järvinen <heikki.jarvinen1@edu.turkuamk.fi>
2024-08-12 16:52:41 +0300

    chore: Add Vite configuration files, tsconfig files, and gitignore file

---
+++ osa9/ts-note-app/src/App.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+interface Note {
+  id: string;
+  content: string;
+}
+
+const App = () => {
+  const [newNote, setNewNote] = useState("");
+  const [notes, setNotes] = useState<Note[]>([
+    { id: "1", content: "test note" },
+  ]);
+
+  const addNote = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    const noteToAdd = {
+      content: newNote,
+      id: String(notes.length + 1),
+    };
+    setNotes(notes.concat(noteToAdd));
+    setNewNote("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={(event) => setNewNote(event.target.value)}
+        />
+        <button type="submit">add</button>
+      </form>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>{note.content}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
